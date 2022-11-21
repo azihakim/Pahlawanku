a9cce8c8cd8a686d181ead631db03cd23c61625a
none <113509874+azihakim@users.noreply.github.com>
2022-11-21 14:54:58 +0700

    21/11/2022

--- app/src/main/java/com/pab/pahlawanku/AdapterCard.java
+++ app/src/main/java/com/pab/pahlawanku/AdapterCard.java
@@ -1,5 +1,7 @@
 package com.pab.pahlawanku;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,15 +18,21 @@ import java.util.ArrayList;
 
 public class AdapterCard extends RecyclerView.Adapter<AdapterCard.ClassViewHolder>{
     private ArrayList<ModelPahlawan> dataPahlawan;
+    private Context ctx;
 
-    public AdapterCard(ArrayList<ModelPahlawan> dataPahlawan) {
+//    public AdapterCard(ArrayList<ModelPahlawan> dataPahlawan) {
+//        this.dataPahlawan = dataPahlawan;
+//    }
+
+    public AdapterCard(ArrayList<ModelPahlawan> dataPahlawan, Context ctx) {
         this.dataPahlawan = dataPahlawan;
+        this.ctx = ctx;
     }
 
     @NonNull
     @Override
     public ClassViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View varView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card, parent, false);
+        View varView = LayoutInflater.from(ctx).inflate(R.layout.item_card, parent, false);
         return new ClassViewHolder(varView);
     }
 
@@ -34,10 +42,28 @@ public class AdapterCard extends RecyclerView.Adapter<AdapterCard.ClassViewHolde
         holder.tvNama.setText(pahlawan.getNama());
         holder.tvTentang.setText(pahlawan.getTentang());
         Glide
-                .with(holder.itemView.getContext())
+//                .with(holder.itemView.getContext())
+                .with(ctx)
                 .load(pahlawan.getFoto())
                 .centerCrop()
                 .into(holder.ivFoto);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String xNama, xTentang, xFoto;
+
+                xNama = pahlawan.getNama();
+                xTentang = pahlawan.getTentang();
+                xFoto = pahlawan.getFoto();
+
+                Intent kirim = new Intent(ctx, DetailActivity.class);
+                kirim.putExtra("xNama", xNama);
+                kirim.putExtra("xTentang", xTentang);
+                kirim.putExtra("xFoto", xFoto);
+                ctx.startActivity(kirim);
+            }
+        });
     }
 
     @Override
